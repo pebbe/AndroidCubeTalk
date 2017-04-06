a31aa09dcdf53945b535a58455fec96729af389b
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2017-04-06 12:39:49 +0200

    server1

---
+++ go/server1/controller.go
@@ -0,0 +1,404 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
+const (
+	cntrSelfZ = iota
+	cntrEnterExit
+	cntrMoveto
+	cntrLookat
+	cntrColor
+	cntrInfo
+	cntrCubesize
+	cntrHead
+	cntrFace
+	cntrAudio
+	numberOfCtrs // this one MUST be last
+)
+
+var (
+	started = false
+)
+
+func controller() {
+
+	initFaces()
+	initSize()
+	initNodding()
+	initShaking()
+	initTilting()
+	initLooking()
+
+	for {
+		select {
+		case <-chQuit:
+			return
+		case req := <-chIn:
+			chLog <- "R " + req.uid + " " + req.req
+			handleReq(req)
+		case cmd := <-chCmd:
+			chLog <- "C " + cmd
+			handleCmd(cmd)
+		}
+	}
+}
+
+// handleReq is not run concurrently, so it must be fast
+func handleReq(req tRequest) {
+
+	cmd := req.req
+	idx := req.idx
+	ch := chOut[idx]
+	user := users[idx]
+
+	words := strings.Fields(cmd)
+	switch words[0] {
+
+	case "log":
+
+		fmt.Println("Log", req.uid+":", strings.Join(words[1:], " "))
+
+	case "reset":
+
+		// This commands tells the server that the client needs the setup.
+		// The setup is not sent now, but with the following 'lookat' command.
+
+		user.needSetup = true
+
+		resetSize(idx)
+
+		resetLooking(idx)
+
+		resetFaces(idx)
+
+	case "lookat":
+
+		if !started {
+			return
+		}
+
+		if len(words) != 6 && len(words) != 7 {
+			w(fmt.Errorf("Invalid number of arguments from %q: %s", req.uid, cmd))
+			return
+		}
+
+		X, err := strconv.ParseFloat(words[1], 64)
+		if w(err) != nil {
+			return
+		}
+		Y, err := strconv.ParseFloat(words[2], 64)
+		if w(err) != nil {
+			return
+		}
+		Z, err := strconv.ParseFloat(words[3], 64)
+		if w(err) != nil {
+			return
+		}
+		roll, err := strconv.ParseFloat(words[4], 64)
+		if w(err) != nil {
+			return
+		}
+
+		if useAudio {
+			/*
+				audio, err := strconv.ParseFloat(words[5], 64)
+				if w(err) != nil {
+					return
+				}
+			*/
+		}
+
+		// only one goroutine modifying these variables, so no sync needed
+		user.lookat.x = X
+		user.lookat.y = Y
+		user.lookat.z = Z
+		user.roll = roll
+
+		marked := len(words) == 7
+		if marked {
+			fmt.Printf("Mark %s -> %g %g %g    %.0f° right   %.0f° up\n",
+				req.uid,
+				X, Y, Z,
+				math.Atan2(X, -Z)/math.Pi*180,
+				math.Atan2(Y, math.Sqrt(X*X+Z*Z))/math.Pi*180)
+		}
+
+		if user.needSetup {
+			// this must be in one batch to make sure that the order is preserved
+			var buf bytes.Buffer
+			user.n[cntrSelfZ]++
+			fmt.Fprintf(&buf, "self %d %g\n", user.n[cntrSelfZ], user.selfZ)
+			if useAudio {
+				user.n[cntrAudio]++
+				fmt.Fprintf(&buf, "audio %d on\n", user.n[cntrAudio])
+			}
+			for _, cube := range user.cubes {
+				if cube != nil {
+					user.n[cntrEnterExit]++
+					fmt.Fprintf(&buf, "enter %s %d\n", cube.uid, user.n[cntrEnterExit])
+					user.n[cntrMoveto]++
+					fmt.Fprintf(&buf, "moveto %s %d %g %g %g\n", cube.uid, user.n[cntrMoveto], cube.pos.x, cube.pos.y, cube.pos.z)
+					user.n[cntrColor]++
+					fmt.Fprintf(&buf, "color %s %d %g %g %g\n", cube.uid, user.n[cntrColor], cube.color.r, cube.color.g, cube.color.b)
+					user.n[cntrHead]++
+					fmt.Fprintf(&buf, "head %s %d %d\n", cube.uid, user.n[cntrHead], cube.head)
+				}
+			}
+			ch <- buf.String()
+			user.needSetup = false
+		}
+
+		user.n[cntrLookat]++
+		for i, cube := range user.cubes {
+
+			if cube != nil {
+
+				if marked && isLookingAt(idx, i) {
+					chLog <- fmt.Sprintf("I Mark %s -> %s", req.uid, cube.uid)
+					fmt.Printf("Mark %s -> %s\n", req.uid, cube.uid)
+					marked = false
+				}
+
+				l := users[i].lookat
+				f := cube.forward
+
+				// assumption: forward is horizontal
+
+				rotH := math.Atan2(l.x, l.z) - math.Atan2(f.x, -f.z)
+				rotV := math.Atan2(l.y, math.Sqrt(l.x*l.x+l.z*l.z))
+				tilt := users[i].roll
+
+				rotH = doShake(idx, i, rotH)
+				rotV = doNod(idx, i, rotV)
+				tilt = doTilt(idx, i, tilt)
+
+				ch <- fmt.Sprintf("lookat %s %d %g %g %g %g\n",
+					cube.uid,
+					user.n[cntrLookat],
+					math.Sin(rotH)*math.Cos(rotV),
+					math.Sin(rotV),
+					math.Cos(rotH)*math.Cos(rotV),
+					tilt)
+			}
+		}
+
+		showLooking(ch, idx)
+
+		showSize(ch, idx)
+
+		showFaces(ch, idx)
+
+	case "info":
+
+		if len(words) != 3 {
+			w(fmt.Errorf("Invalid number of arguments from %q: %s", req.uid, cmd))
+			return
+		}
+
+		chLog <- fmt.Sprintf("Choice by %s for %s: %s", req.uid, words[1], words[2])
+		fmt.Printf("Choice by %s for %s: %s\n", req.uid, words[1], words[2])
+
+	default:
+
+		w(fmt.Errorf("Invalid command from %q: %s", req.uid, cmd))
+
+	}
+}
+
+// handleCmd is not run concurrently, so it must be fast
+func handleCmd(cmd string) {
+
+	number_args := "Invalid number of arguments from GUI: %s"
+
+	fmt.Println("Command:", cmd)
+
+	words := strings.Fields(cmd)
+	switch words[0] {
+
+	case "start":
+
+		started = true
+
+	case "face":
+
+		if len(words) != 3 {
+			w(fmt.Errorf(number_args, cmd))
+			return
+		}
+
+		idx, ok := labels[words[1]]
+		if !ok {
+			w(fmt.Errorf("Illegal label in command: %s", cmd))
+			return
+		}
+
+		f, err := strconv.ParseInt(words[2], 10, 16)
+		if w(err) != nil {
+			return
+		}
+
+		setFace(idx, int(f))
+
+	case "cubesize":
+
+		if len(words) != 4 {
+			w(fmt.Errorf(number_args, cmd))
+			return
+		}
+		var f [3]float64
+		for i := 0; i < 3; i++ {
+			var err error
+			f[i], err = strconv.ParseFloat(words[i+1], 64)
+			if w(err) != nil {
+				f[i] = 1
+			}
+		}
+		setSize(f[0], f[1], f[2])
+
+	case "recenter":
+
+		// Orders the headset of a particular user to recenter
+		// the direction the head is currently pointing.
+
+		if len(words) != 2 {
+			w(fmt.Errorf(number_args, cmd))
+			return
+		}
+
+		if idx, ok := labels[words[1]]; ok {
+			select {
+			case chOut[idx] <- "recenter\n":
+			default:
+				// channel is full and nobody is reading from the channel
+			}
+		} else {
+			w(fmt.Errorf("Invalid user in command from GUI: %s", cmd))
+		}
+
+	case "nod":
+
+		if len(words) != 4 {
+			w(fmt.Errorf(number_args, cmd))
+			return
+		}
+
+		i, oki := labels[words[1]]
+		j, okj := labels[words[2]]
+		if !(oki && okj) {
+			w(fmt.Errorf("Invalid users in command from GUI: %s", cmd))
+			return
+		}
+		f, err := strconv.ParseFloat(words[3], 64)
+		if w(err) != nil {
+			return
+		}
+		setNod(i, j, f)
+
+	case "globalnod":
+
+		// Set amplification of nodding for all users, for all cubes they see
+
+		if len(words) != 2 {
+			w(fmt.Errorf(number_args, cmd))
+			return
+		}
+		f, err := strconv.ParseFloat(words[1], 64)
+		if w(err) != nil {
+			return
+		}
+		setNodAll(f)
+
+	case "shake":
+
+		if len(words) != 4 {
+			w(fmt.Errorf(number_args, cmd))
+			return
+		}
+
+		i, oki := labels[words[1]]
+		j, okj := labels[words[2]]
+		if !(oki && okj) {
+			w(fmt.Errorf("Invalid users in command from GUI: %s", cmd))
+			return
+		}
+		f, err := strconv.ParseFloat(words[3], 64)
+		if w(err) != nil {
+			return
+		}
+		setShake(i, j, f)
+
+	case "globalshake":
+
+		// Set amplification of shaking for all users, for all cubes they see
+
+		if len(words) != 2 {
+			w(fmt.Errorf(number_args, cmd))
+			return
+		}
+		f, err := strconv.ParseFloat(words[1], 64)
+		if w(err) != nil {
+			return
+		}
+		setShakeAll(f)
+
+	case "tilt":
+
+		if len(words) != 4 {
+			w(fmt.Errorf(number_args, cmd))
+			return
+		}
+
+		i, oki := labels[words[1]]
+		j, okj := labels[words[2]]
+		if !(oki && okj) {
+			w(fmt.Errorf("Invalid users in command from GUI: %s", cmd))
+			return
+		}
+		f, err := strconv.ParseFloat(words[3], 64)
+		if w(err) != nil {
+			return
+		}
+		setTilt(i, j, f)
+
+	case "globaltilt":
+
+		// Set amplification of tilting for all users, for all cubes they see
+
+		if len(words) != 2 {
+			w(fmt.Errorf(number_args, cmd))
+			return
+		}
+		f, err := strconv.ParseFloat(words[1], 64)
+		if w(err) != nil {
+			return
+		}
+		setTiltAll(f)
+
+	case "turn":
+
+		if len(words) != 5 {
+			w(fmt.Errorf(number_args, cmd))
+			return
+		}
+
+		i, oki := labels[words[1]]
+		j, okj := labels[words[2]]
+		k, okk := labels[words[3]]
+		if !(oki && okj && okk) {
+			w(fmt.Errorf("Invalid users in command from GUI: %s", cmd))
+			return
+		}
+		setTurn(i, j, k, words[4] == "on")
+
+	default:
+
+		w(fmt.Errorf("Invalid command from GUI: %s", cmd))
+
+	}
+}
---
+++ go/server1/gui.go
@@ -0,0 +1,408 @@
+package main
+
+import (
+	"github.com/nsf/gothic"
+
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+var (
+	tk *gothic.Interpreter
+)
+
+func gui() {
+
+	tk = gothic.NewInterpreter("namespace eval go {}")
+
+	x(tk.Eval(`
+
+wm title . "` + tclquote(filepath.Base(os.Args[0])) + `"
+
+frame .cmd
+pack .cmd
+label .cmd.l -text {external command:}
+set runcmd {mplayer -quiet ` + filepath.Join(filepath.Dir(os.Args[0]), "cling.mp3") + `}
+entry .cmd.e -textvariable runcmd -width 40
+button .cmd.b -text {run} -command {go::runcommand $runcmd}
+pack .cmd.l .cmd.e .cmd.b -side left
+
+frame .sizes
+pack .sizes
+label .sizes.l -text {size of cubes, width / height / depth:}
+set cubew 1
+set cubeh 1
+set cubed 1
+entry .sizes.w -textvariable cubew
+entry .sizes.h -textvariable cubeh
+entry .sizes.d -textvariable cubed
+button .sizes.b -text {submit} -command {go::cubesize $cubew $cubeh $cubed}
+pack .sizes.l .sizes.w .sizes.h .sizes.d .sizes.b -side left
+
+set fAval ` + fmt.Sprint(cubes[0].face) + `
+frame .fA
+pack .fA
+label .fA.l -text {face A:}
+entry .fA.e  -textvariable fAval
+button .fA.b -text {submit} -command {go::face A $fAval}
+pack .fA.l .fA.e .fA.b -side left
+
+set fBval ` + fmt.Sprint(cubes[1].face) + `
+frame .fB
+pack .fB
+label .fB.l -text {face B:}
+entry .fB.e  -textvariable fBval
+button .fB.b -text {submit} -command {go::face B $fBval}
+pack .fB.l .fB.e .fB.b -side left
+
+set fCval ` + fmt.Sprint(cubes[2].face) + `
+frame .fC
+pack .fC
+label .fC.l -text {face C:}
+entry .fC.e  -textvariable fCval
+button .fC.b -text {submit} -command {go::face C $fCval}
+pack .fC.l .fC.e .fC.b -side left
+
+button .cA -text {recenter A} -command {go::recenter A}
+button .cB -text {recenter B} -command {go::recenter B}
+button .cC -text {recenter C} -command {go::recenter C}
+pack .cA .cB .cC
+
+frame .nst
+pack .nst
+
+frame .nst.nod -relief groove -borderwidth 3 -padx 4 -pady 4
+frame .nst.shake -relief groove -borderwidth 3 -padx 4 -pady 4
+frame .nst.tilt -relief groove -borderwidth 3 -padx 4 -pady 4
+
+pack .nst.nod .nst.shake .nst.tilt -side left -padx 4 -pady 4
+
+label .nst.nod.title -text {Nod}
+pack .nst.nod.title
+
+label .nst.shake.title -text {Shake}
+pack .nst.shake.title
+
+label .nst.tilt.title -text {Tilt}
+pack .nst.tilt.title
+
+frame .nst.nod.nAB
+pack .nst.nod.nAB
+label .nst.nod.nAB.l -text {A sees B:}
+set nodAB 1
+entry .nst.nod.nAB.e -textvariable nodAB
+button .nst.nod.nAB.b -text {submit} -command {go::nod A B $nodAB}
+pack .nst.nod.nAB.l .nst.nod.nAB.e .nst.nod.nAB.b -side left
+
+frame .nst.nod.nAC
+pack .nst.nod.nAC
+label .nst.nod.nAC.l -text {A sees C:}
+set nodAC 1
+entry .nst.nod.nAC.e -textvariable nodAC
+button .nst.nod.nAC.b -text {submit} -command {go::nod A C $nodAC}
+pack .nst.nod.nAC.l .nst.nod.nAC.e .nst.nod.nAC.b -side left
+
+frame .nst.nod.nBA
+pack .nst.nod.nBA
+label .nst.nod.nBA.l -text {B sees A:}
+set nodBA 1
+entry .nst.nod.nBA.e -textvariable nodBA
+button .nst.nod.nBA.b -text {submit} -command {go::nod B A $nodBA}
+pack .nst.nod.nBA.l .nst.nod.nBA.e .nst.nod.nBA.b -side left
+
+frame .nst.nod.nBC
+pack .nst.nod.nBC
+label .nst.nod.nBC.l -text {B sees C:}
+set nodBC 1
+entry .nst.nod.nBC.e -textvariable nodBC
+button .nst.nod.nBC.b -text {submit} -command {go::nod B C $nodBC}
+pack .nst.nod.nBC.l .nst.nod.nBC.e .nst.nod.nBC.b -side left
+
+frame .nst.nod.nCA
+pack .nst.nod.nCA
+label .nst.nod.nCA.l -text {C sees A:}
+set nodCA 1
+entry .nst.nod.nCA.e -textvariable nodCA
+button .nst.nod.nCA.b -text {submit} -command {go::nod C A $nodCA}
+pack .nst.nod.nCA.l .nst.nod.nCA.e .nst.nod.nCA.b -side left
+
+frame .nst.nod.nCB
+pack .nst.nod.nCB
+label .nst.nod.nCB.l -text {C sees B:}
+set nodCB 1
+entry .nst.nod.nCB.e -textvariable nodCB
+button .nst.nod.nCB.b -text {submit} -command {go::nod C B $nodCB}
+pack .nst.nod.nCB.l .nst.nod.nCB.e .nst.nod.nCB.b -side left
+
+frame .nst.nod.r
+pack .nst.nod.r
+label .nst.nod.r.l -text {global:}
+set nodvalue 1
+entry .nst.nod.r.e -textvariable nodvalue
+button .nst.nod.r.b -text {submit} -command {setglobalnod $nodvalue}
+pack .nst.nod.r.l .nst.nod.r.e .nst.nod.r.b -side left
+
+proc setglobalnod args {
+    global nodAB nodAC nodBA nodBC nodCA nodCB
+    set nod [lindex $args 0]
+    set nodAB $nod
+    set nodAC $nod
+    set nodBA $nod
+    set nodBC $nod
+    set nodCA $nod
+    set nodCB $nod
+    go::globalnod $nod
+}
+
+
+
+frame .nst.shake.nAB
+pack .nst.shake.nAB
+label .nst.shake.nAB.l -text {A sees B:}
+set shakeAB 1
+entry .nst.shake.nAB.e -textvariable shakeAB
+button .nst.shake.nAB.b -text {submit} -command {go::shake A B $shakeAB}
+pack .nst.shake.nAB.l .nst.shake.nAB.e .nst.shake.nAB.b -side left
+
+frame .nst.shake.nAC
+pack .nst.shake.nAC
+label .nst.shake.nAC.l -text {A sees C:}
+set shakeAC 1
+entry .nst.shake.nAC.e -textvariable shakeAC
+button .nst.shake.nAC.b -text {submit} -command {go::shake A C $shakeAC}
+pack .nst.shake.nAC.l .nst.shake.nAC.e .nst.shake.nAC.b -side left
+
+frame .nst.shake.nBA
+pack .nst.shake.nBA
+label .nst.shake.nBA.l -text {B sees A:}
+set shakeBA 1
+entry .nst.shake.nBA.e -textvariable shakeBA
+button .nst.shake.nBA.b -text {submit} -command {go::shake B A $shakeBA}
+pack .nst.shake.nBA.l .nst.shake.nBA.e .nst.shake.nBA.b -side left
+
+frame .nst.shake.nBC
+pack .nst.shake.nBC
+label .nst.shake.nBC.l -text {B sees C:}
+set shakeBC 1
+entry .nst.shake.nBC.e -textvariable shakeBC
+button .nst.shake.nBC.b -text {submit} -command {go::shake B C $shakeBC}
+pack .nst.shake.nBC.l .nst.shake.nBC.e .nst.shake.nBC.b -side left
+
+frame .nst.shake.nCA
+pack .nst.shake.nCA
+label .nst.shake.nCA.l -text {C sees A:}
+set shakeCA 1
+entry .nst.shake.nCA.e -textvariable shakeCA
+button .nst.shake.nCA.b -text {submit} -command {go::shake C A $shakeCA}
+pack .nst.shake.nCA.l .nst.shake.nCA.e .nst.shake.nCA.b -side left
+
+frame .nst.shake.nCB
+pack .nst.shake.nCB
+label .nst.shake.nCB.l -text {C sees B:}
+set shakeCB 1
+entry .nst.shake.nCB.e -textvariable shakeCB
+button .nst.shake.nCB.b -text {submit} -command {go::shake C B $shakeCB}
+pack .nst.shake.nCB.l .nst.shake.nCB.e .nst.shake.nCB.b -side left
+
+frame .nst.shake.r
+pack .nst.shake.r
+label .nst.shake.r.l -text {global:}
+set shakevalue 1
+entry .nst.shake.r.e -textvariable shakevalue
+button .nst.shake.r.b -text {submit} -command {setglobalshake $shakevalue}
+pack .nst.shake.r.l .nst.shake.r.e .nst.shake.r.b -side left
+
+proc setglobalshake args {
+    global shakeAB shakeAC shakeBA shakeBC shakeCA shakeCB
+    set shake [lindex $args 0]
+    set shakeAB $shake
+    set shakeAC $shake
+    set shakeBA $shake
+    set shakeBC $shake
+    set shakeCA $shake
+    set shakeCB $shake
+    go::globalshake $shake
+}
+
+
+
+frame .nst.tilt.nAB
+pack .nst.tilt.nAB
+label .nst.tilt.nAB.l -text {A sees B:}
+set tiltAB 1
+entry .nst.tilt.nAB.e -textvariable tiltAB
+button .nst.tilt.nAB.b -text {submit} -command {go::tilt A B $tiltAB}
+pack .nst.tilt.nAB.l .nst.tilt.nAB.e .nst.tilt.nAB.b -side left
+
+frame .nst.tilt.nAC
+pack .nst.tilt.nAC
+label .nst.tilt.nAC.l -text {A sees C:}
+set tiltAC 1
+entry .nst.tilt.nAC.e -textvariable tiltAC
+button .nst.tilt.nAC.b -text {submit} -command {go::tilt A C $tiltAC}
+pack .nst.tilt.nAC.l .nst.tilt.nAC.e .nst.tilt.nAC.b -side left
+
+frame .nst.tilt.nBA
+pack .nst.tilt.nBA
+label .nst.tilt.nBA.l -text {B sees A:}
+set tiltBA 1
+entry .nst.tilt.nBA.e -textvariable tiltBA
+button .nst.tilt.nBA.b -text {submit} -command {go::tilt B A $tiltBA}
+pack .nst.tilt.nBA.l .nst.tilt.nBA.e .nst.tilt.nBA.b -side left
+
+frame .nst.tilt.nBC
+pack .nst.tilt.nBC
+label .nst.tilt.nBC.l -text {B sees C:}
+set tiltBC 1
+entry .nst.tilt.nBC.e -textvariable tiltBC
+button .nst.tilt.nBC.b -text {submit} -command {go::tilt B C $tiltBC}
+pack .nst.tilt.nBC.l .nst.tilt.nBC.e .nst.tilt.nBC.b -side left
+
+frame .nst.tilt.nCA
+pack .nst.tilt.nCA
+label .nst.tilt.nCA.l -text {C sees A:}
+set tiltCA 1
+entry .nst.tilt.nCA.e -textvariable tiltCA
+button .nst.tilt.nCA.b -text {submit} -command {go::tilt C A $tiltCA}
+pack .nst.tilt.nCA.l .nst.tilt.nCA.e .nst.tilt.nCA.b -side left
+
+frame .nst.tilt.nCB
+pack .nst.tilt.nCB
+label .nst.tilt.nCB.l -text {C sees B:}
+set tiltCB 1
+entry .nst.tilt.nCB.e -textvariable tiltCB
+button .nst.tilt.nCB.b -text {submit} -command {go::tilt C B $tiltCB}
+pack .nst.tilt.nCB.l .nst.tilt.nCB.e .nst.tilt.nCB.b -side left
+
+frame .nst.tilt.r
+pack .nst.tilt.r
+label .nst.tilt.r.l -text {global:}
+set tiltvalue 1
+entry .nst.tilt.r.e -textvariable tiltvalue
+button .nst.tilt.r.b -text {submit} -command {setglobaltilt $tiltvalue}
+pack .nst.tilt.r.l .nst.tilt.r.e .nst.tilt.r.b -side left
+
+proc setglobaltilt args {
+    global tiltAB tiltAC tiltBA tiltBC tiltCA tiltCB
+    set tilt [lindex $args 0]
+    set tiltAB $tilt
+    set tiltAC $tilt
+    set tiltBA $tilt
+    set tiltBC $tilt
+    set tiltCA $tilt
+    set tiltCB $tilt
+    go::globaltilt $tilt
+}
+
+
+frame .t
+pack .t
+
+frame .t.a -relief groove -borderwidth 3 -padx 4 -pady 4
+frame .t.b -relief groove -borderwidth 3 -padx 4 -pady 4
+frame .t.c -relief groove -borderwidth 3 -padx 4 -pady 4
+
+pack .t.a .t.b .t.c -side left -padx 4 -pady 4
+
+set abc off
+set acb off
+label .t.a.title -text {A sees...}
+checkbutton .t.a.bc -variable abc -onvalue on -offvalue off -text {B looking at C} -command {go::turn A B C $abc}
+checkbutton .t.a.cb -variable acb -onvalue on -offvalue off -text {C looking at B} -command {go::turn A C B $acb}
+pack .t.a.title .t.a.bc .t.a.cb
+
+set bac off
+set bca off
+label .t.b.title -text {B sees...}
+checkbutton .t.b.ac -variable bac -onvalue on -offvalue off -text {A looking at C} -command {go::turn B A C $bac}
+checkbutton .t.b.ca -variable bca -onvalue on -offvalue off -text {C looking at A} -command {go::turn B C A $bca}
+pack .t.b.title .t.b.ac .t.b.ca
+
+set cab off
+set cba off
+label .t.c.title -text {C sees...}
+checkbutton .t.c.ab -variable cab -onvalue on -offvalue off -text {A looking at B} -command {go::turn C A B $cab}
+checkbutton .t.c.ba -variable cba -onvalue on -offvalue off -text {B looking at A} -command {go::turn C B A $cba}
+pack .t.c.title .t.c.ab .t.c.ba
+
+button .start -text { START } -command { go::start ; destroy .start } -background {#00a000} -foreground white
+pack .start -expand yes -fill x -padx 32 -pady 8
+
+# Don't use command exit, because that will kill Go as well
+button .q -text {exit} -command {destroy .}
+pack .q -side left
+
+`))
+
+	x(tk.RegisterCommand("go::runcommand", func(command string) {
+		fmt.Println("Command: run", command)
+		chLog <- "C run begin: " + command
+		args := strings.Fields(command)
+		cmd := exec.Command(args[0], args[1:]...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		if w(err) != nil {
+			fmt.Println(err)
+		}
+		chLog <- "C run end: " + command
+	}))
+
+	x(tk.RegisterCommand("go::start", func() {
+		chCmd <- "start"
+	}))
+
+	x(tk.RegisterCommand("go::cubesize", func(w, h, d string) {
+		chCmd <- "cubesize " + w + " " + h + " " + d
+	}))
+
+	x(tk.RegisterCommand("go::recenter", func(s string) {
+		chCmd <- "recenter " + s
+	}))
+
+	x(tk.RegisterCommand("go::nod", func(sees, seen, value string) {
+		chCmd <- "nod " + sees + " " + seen + " " + value
+	}))
+
+	x(tk.RegisterCommand("go::globalnod", func(s string) {
+		chCmd <- "globalnod " + s
+	}))
+
+	x(tk.RegisterCommand("go::shake", func(sees, seen, value string) {
+		chCmd <- "shake " + sees + " " + seen + " " + value
+	}))
+
+	x(tk.RegisterCommand("go::globalshake", func(s string) {
+		chCmd <- "globalshake " + s
+	}))
+
+	x(tk.RegisterCommand("go::tilt", func(sees, seen, value string) {
+		chCmd <- "tilt " + sees + " " + seen + " " + value
+	}))
+
+	x(tk.RegisterCommand("go::globaltilt", func(s string) {
+		chCmd <- "globaltilt " + s
+	}))
+
+	x(tk.RegisterCommand("go::turn", func(sees, seen, seeing, val string) {
+		chCmd <- "turn " + sees + " " + seen + " " + seeing + " " + val
+	}))
+
+	x(tk.RegisterCommand("go::face", func(uid, idx string) {
+		chCmd <- "face " + uid + " " + idx
+	}))
+
+	<-tk.Done
+}
+
+func tclquote(s string) string {
+	s = strings.Replace(s, "\\", "\\\\", -1)
+	s = strings.Replace(s, "\"", "\\\"", -1)
+	s = strings.Replace(s, "[", "\\[", -1)
+	s = strings.Replace(s, "]", "\\]", -1)
+	s = strings.Replace(s, "$", "\\$", -1)
+	return s
+}
